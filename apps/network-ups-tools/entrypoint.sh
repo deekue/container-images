3aac2ed051f0f6222c107be8c0ba64bb22e84f51
Daniel Quinlan <dq@chaosengine.net>
2022-07-04 15:59:49 -0700

    add support for NUT env var mode, enable netclient mode
    this allows for running in netclient mode and monitoring a remote node
    that has the connected UPS.
    
    this can be controlled by configuring /etc/nut/nut.conf, as in a local
    install, or overriding the MODE env var

--- apps/network-ups-tools/entrypoint.sh
+++ apps/network-ups-tools/entrypoint.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-/sbin/upsdrvctl -u root start
-exec /sbin/upsd -u root -D
+# if one child dies, clean up the other(s)
+trap 'trap " " SIGTERM; kill 0; wait' SIGTERM SIGQUIT SIGINT
+
+if [[ "$MODE" =~ standalone|netserver ]] ; then
+  /sbin/upsdrvctl -u root start
+fi
+
+# these will run or not based on $MODE
+/sbin/upsd -u root -D &
+/sbin/upsmon -D &
+
+wait
